6af785646478647372df20b9d7a9a83f159794ec
Muhammad Salman Abid <ealahherp@gmail.com>
2019-03-25 12:45:13 +0700

    set db

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'api'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'coba_api'),
+            'username' => env('DB_USERNAME', 'abidgans'),
+            'password' => env('DB_PASSWORD', 'semar365'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
